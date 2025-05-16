e450bce9558aa7ecf70919c7d09eabe0502aaa64
Prosper <hazardprosper227@gmail.com>
2025-05-16 15:01:19 +0100

    First commi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+ } from 'react-router-dom';
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import ReadBook from './pages/ReadBook';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' Component={Home}></Route>  
+        <Route path='/collection' Component={Collection}></Route>
+        <Route path='/read/:bookid' Component={ReadBook}> </Route>
+        <Route path='/login' Component={Login}></Route>
+        <Route path='/register' Component={Register}></Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
-export default App;
+export default App;  
